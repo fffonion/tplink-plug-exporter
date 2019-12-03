a8b8f4a6db28d76ba2e77deefdbe712a576f274c
Dennis Urban <urban@belwue.de>
2019-12-03 14:08:49 +0100

    fixed emeter voltage, current and total variables

--- kasa/emeter.go
+++ kasa/emeter.go
@@ -8,10 +8,10 @@ type GetRealtimeRequest struct {
 }
 
 type GetRealtimeResponse struct {
-	Current float64 `mapstructure:"current"`
-	Voltage float64 `mapstructure:"voltage"`
-	Power   float64 `mapstructure:"power"`
-	Total   float64 `mapstructure:"total"`
+	Current float64 `mapstructure:"current_ma"`
+	Voltage float64 `mapstructure:"voltage_mv"`
+	Power   float64 `mapstructure:"power_mw"`
+	Total   float64 `mapstructure:"total_wh"`
 }
 
 func (s *KasaClientEmeterService) GetRealtime() (*GetRealtimeResponse, error) {
