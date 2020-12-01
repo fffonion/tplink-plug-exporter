24372466450de07cd66c4c0bd79a723627bd9a86
Wangchong Zhou <fffonion@gmail.com>
2020-12-01 16:08:13 +0800

    feat: add deviceId to all metrics, add kasa_metadata

--- kasa/system.go
+++ kasa/system.go
@@ -11,21 +11,25 @@ type GetSysInfoRequest struct {
 }
 
 type SysInfoChildren struct {
-	ID    string `mapstructure:"id"`
-	State int    `mapstructure:"state"`
-	Alias string `mapstructure:"alias"`
+	ID     string `mapstructure:"id"`
+	State  int    `mapstructure:"state"`
+	Alias  string `mapstructure:"alias"`
+	OnTime int    `mapstructure:"on_time"`
 }
 
 type GetSysInfoResponse struct {
-	MAC        string            `mapstructure:"mac"`
-	Model      string            `mapstructure:"model"`
-	Alias      string            `mapstructure:"alias"`
-	Feature    string            `mapstructure:"feature"`
-	RelayState int               `mapstructure:"relay_state"`
-	RSSI       int               `mapstructure:"rssi"`
-	LEDOff     int               `mapstructure:"led_off"`
-	OnTime     int               `mapstructure:"on_time"`
-	Children   []SysInfoChildren `mapstructure:"children"`
+	MAC             string            `mapstructure:"mac"`
+	Model           string            `mapstructure:"model"`
+	Alias           string            `mapstructure:"alias"`
+	Feature         string            `mapstructure:"feature"`
+	RelayState      int               `mapstructure:"relay_state"`
+	RSSI            int               `mapstructure:"rssi"`
+	LEDOff          int               `mapstructure:"led_off"`
+	OnTime          int               `mapstructure:"on_time"`
+	DeviceID        string            `mapstructure:"deviceId"`
+	SoftwareVersion string            `mapstructure:"sw_ver"`
+	HardwareVersion string            `mapstructure:"hw_ver"`
+	Children        []SysInfoChildren `mapstructure:"children"`
 }
 
 func (s *KasaClientSystemService) GetSysInfo() (*GetSysInfoResponse, error) {
